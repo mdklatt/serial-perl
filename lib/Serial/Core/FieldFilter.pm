cb2548358a166a76424b1c399832795de0a3107a
Michael Klatt <mdklatt@ou.edu>
2014-07-27 17:41:42 -0500

    refactored FieldFilter to use _RecordFilter base class

--- lib/Serial/Core/FieldFilter.pm
+++ lib/Serial/Core/FieldFilter.pm
@@ -1,34 +1,7 @@
 package Serial::Core::FieldFilter;
-
+use parent qw(Serial::Core::_RecordFilter);
 use strict;
 use warnings;
-
-
-# Create a new object.
-#
-# See _init() for an argument description.
-#
-sub new {
-    # Derived classes should not need to override this class; override _init()
-    # to handle class-specific initialization.
-    my $class = shift @_;
-    my $self = bless {}, $class;
-    $self->_init(@_);
-    return $self;
-}
-
-# Execute the filter.
-#
-sub call {
-    my $self = shift @_;
-    my ($record) = @_;
-    my $match = 0;
-    if (exists $record->{$self->{_field}}) {
-        my $value = $record->{$self->{_field}};
-        $match = $self->{_values}{$value};
-    }
-    return ($match xor $self->{_blacklist}) ? $record : undef;
-}
 
 # Initialize this object.
 #
@@ -40,10 +13,16 @@ sub _init {
     # This is called by new() to do the real work when an object is created.
     # Derived classes may override this as necessary.
     my $self = shift @_;
-    ($self->{_field}, my $values, my %opts) = @_;
+    my ($field, $values, %opts) = @_;
+    $self->SUPER::_init($field, %opts);
     $self->{_values} = {map { $_ => 1 } @$values};  # pseudo set
-    $self->{_blacklist} = $opts{blacklist};
     return;
+}
+
+sub _match {
+    my $self = shift @_;
+    my ($value) = @_;
+    return $self->{_values}{$value};
 }
 
 1;
