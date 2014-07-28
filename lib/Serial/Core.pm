9b238e72625fcd9d5811e8212743d2b22f1577d4
Michael Klatt <mdklatt@ou.edu>
2014-07-27 22:34:35 -0500

    modified Core.pm module

--- lib/Serial/Core.pm
+++ lib/Serial/Core.pm
@@ -1,10 +1,15 @@
 package Serial::Core;
 
+use 5.008;
 use strict;
 use warnings;
+
+our $VERSION = eval '0.001'; 
 
 use Serial::Core::ScalarField;
 use Serial::Core::DelimitedReader;
 use Serial::Core::FixedWidthReader;
+use Serial::Core::FieldFilter;
+use Serial::Core::RangeFilter;
 
 1;
