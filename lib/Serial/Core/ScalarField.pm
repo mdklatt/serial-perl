cf8e92d298a5c48f536c6393661cfeb5ec4bb422
Michael Klatt <mdklatt@ou.edu>
2014-08-14 21:00:03 -0500

    added POD documentation to ScalarField.pm

--- lib/Serial/Core/ScalarField.pm
+++ lib/Serial/Core/ScalarField.pm
@@ -76,3 +76,78 @@ sub _init {
 }    
 
 1;
+
+__END__
+
+=pod 
+
+=encoding utf8
+
+=head1 NAME
+
+Serial::Core::ScalarField - define a scalar data field
+
+
+=head1 SYNOPSIS
+
+    use Serial::Core;
+
+    my @fields = (
+        Serial::Core::ScalarField->new($name, $pos),
+        ...
+    );
+    my $reader = new Serial::Core::DelimitedReader($stream, \@fields);
+
+    my @fields = (
+        Serial::Core::ScalarField->new($name, [$beg, $len]),
+        ...
+    );
+    my $reader = new Serial::Core::FixedWidthReader($stream, \@fields);
+
+
+=head1 DESCRIPTION
+
+A I<ScalarField> maps a single input/output token to a data field. I<Reader>s
+and I<Writer>s are initialized using one or more field specifiers that define
+the layout of their data stream.
+
+=head2 CLASS METHODS
+
+=over
+
+=item new($name, $pos, fmt => '%s', quote => '', default => undef)
+
+Create a new field. The first argument is the name of the field, which is used
+to refer to the field in a data record, e.g. C<$record-E<gt>{$name}> The next 
+argument is the field position in the input/output line. For delimited data
+this is the field index (starting at 0), and for fixed-width data this is the 
+substring occupied by the field as given by its offset from 0 and total width
+(inclusive of any spacing between fields). Fixed-width fields are padded on 
+the left or trimmed on the right to fit their allotted width on output. 
+
+The optional named arguments are formatting options. The I<fmt> argument is a 
+C<printf()> format string that is used by a I<Writer> for formatted output (it
+has no effect on input). Specifying a format width is optional, but for
+fixed-width fields a format width smaller than the field width can be used to
+control spacing between fields. Specify a I<quote> character to strip input
+of leading/trailing quotes and to automatically quote output. The I<default>
+value is used for null/missing fields.
+
+=back
+
+=head2 OBJECT METHODS
+
+The object methods are used by I<Reader>s and I<Writer>s; there is no need to
+access a I<ScalarField> object directly.
+
+=head2 OBJECT ATTRIBUTES
+
+The object attributes are used by I<Reader>s and I<Writer>s; there is no need 
+to access a I<ScalarField> object directly.
+
+
+=head1 EXPORTS
+
+The I<Serial::Core> library makes this class available by default.
+
+=cut
